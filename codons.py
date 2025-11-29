023a9b6f240f5e0fb3531df558b62baa865bbfd3
noamsalo <noamsal30@gmail.com>
2025-11-29 16:28:15 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+   def create_codon_dict(file_path):
+  codon_dict = {}
+  file = open(path)
+  rows = file.readlines()
+  file.close()
+  for line in rows:
+    parts = line.strip().split('\t')
+    codon = parts[0]
+    amino_acid_abbreviation = parts[2]
+    codon_dict[codon] = amino_acid_abbreviation
+  return codon_dict
 
 
