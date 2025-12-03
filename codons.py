6bc0a34fd0e48ae45ee57a598df73276d0d5c55d
noamsalo <noamsal30@gmail.com>
2025-12-03 12:27:05 +0200

    Refactor create_codon_dict for clarity and correctness

--- codons.py
+++ codons.py
@@ -1,14 +1,15 @@
 def create_codon_dict(file_path):
-   def create_codon_dict(file_path):
-  codon_dict = {}
-  file = open(path)
-  rows = file.readlines()
-  file.close()
-  for line in rows:
-    parts = line.strip().split('\t')
-    codon = parts[0]
-    amino_acid_abbreviation = parts[2]
-    codon_dict[codon] = amino_acid_abbreviation
-  return codon_dict
+    codon_dict = {}
 
+    file = open(file_path, "r")
+    rows = file.readlines()
+    file.close()
 
+    
+    for line in rows:
+        parts = line.strip().split('\t')
+        codon = parts[0]                     
+        amino_acid_abbreviation = parts[2]   
+        codon_dict[codon] = amino_acid_abbreviation
+
+    return codon_dict
